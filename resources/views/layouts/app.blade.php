9c17a27a8ad6bfe227e07d7d0e1460646144edf0
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2026-04-20 15:49:04 +0300

    Change Google Analytics ID in app.blade.php
    Updated Google Analytics tracking ID in app layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,13 +111,15 @@
 
     {{-- Google Analytics --}}
     @production
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
-        </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JDQTSGVSYS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JDQTSGVSYS');
+</script>
     @endproduction
 
 </body>
