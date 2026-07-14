371f1ab7e756abb73b23e3a71b6e5f4ad5a5ae23
Dante-VIQ <damalide20@gmail.com>
2026-07-14 13:46:06 +0300

    alpine fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,5 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
 document.addEventListener('DOMContentLoaded', function () {
     const track = document.getElementById('slider-track');
     const slides = document.querySelectorAll('#slider-track > div');
