d551ee33b9c7d5ed3088c15c9cd66ea6f0489622
Dante-VIQ <damalide20@gmail.com>
2026-09-24 11:22:35 +0300

    uploads blogs bug

--- resources/views/components/⚡view-blog.blade.php
+++ resources/views/components/⚡view-blog.blade.php
@@ -317,7 +317,7 @@ new class extends Component
     @if ($blog->media_path)
         <div class="container mx-auto px-6 mb-12">
             <div class="max-w-5xl mx-auto">
-                <img src="{{ $blog->media_path }}" alt="{{ $blog->title }}"
+                <img src="{{ asset('uploads/' . $blog->media_path) }}" alt="{{ $blog->title }}"
                     class="w-full rounded-3xl shadow-xl object-cover max-h-[600px] border border-black/5">
             </div>
         </div>
